ac0e788378ef5cb077e0e00e279a0c57dcaee232
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put xos variables in the "Xos" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -2,6 +2,7 @@ xos_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Xos": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
@@ -13,5 +14,6 @@ xos_soong:
 	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
 	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),';  \
 	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false),';  \
+	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false)';  \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
