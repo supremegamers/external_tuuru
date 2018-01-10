LineageOS/android_device_lineage_sepolicy

sepolicy: Include platform specific sepolicy
Change-Id: Ib2c58c95f934477e782fec64d2aa4dc951d69b9d

qcom/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom
+    device/lineage/sepolicy/qcom/common \
+    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)

supremegamers/external_tuuru

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

sepolicy/qcom/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    external/xos/sepolicy/qcom/
+    external/xos/sepolicy/qcom/common \
+    external/xos/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)