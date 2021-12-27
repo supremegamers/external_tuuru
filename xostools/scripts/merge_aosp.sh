615dbbdcb047c8b0072ee53b0bbce70fc7564a5c
Simão Gomes Viana <devel@superboring.dev>
2021-12-28 00:29:55 +0100

    xostools: merge_aosp: fixup
    Change-Id: I57c66e48230b3f0e3871e373b1fbd3c3265490fa

--- xostools/scripts/merge_aosp.sh
+++ xostools/scripts/merge_aosp.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd $TOP
+cd "$TOP"
 
 snippet="$TOP/.repo/manifests/snippets/XOS.xml"
 
@@ -11,13 +11,13 @@ if [[ "$2" == "-"* ]]; then
     exit 2
 fi
 
-source build/envsetup.sh
+source "build/envsetup.sh"
 
 if [ "$1" != "--no-reset" ]; then
     echo "Warning: This will perform a reporeset and a reposync to make sure everything is up to date before doing the merges"
     echo "If you do not want that to happen, abort now using CTRL+C and use the parameter --no-reset"
     echo "Otherwise, just confirm with ENTER"
-    read
+    read -r
     echo
     reporeset
     reposync fast
@@ -32,49 +32,45 @@ fi
 
 revision="$1"
 
-cd $TOP
-
-while read path; do
+while read -r path; do
     echo "$path"
-    repo_path="$path"
     repo_name=$(xmlstarlet sel -t -v "/manifest/project[@path='$path']/@name" $snippet)
     repo_name_aosp=$(echo "$repo_name" | sed -e "s/android_//" -e "s/^build_make$/build/" -e "s/_/\//g" -e 's/PermissionController/PackageInstaller/')
     repo_aosp="https://android.googlesource.com/platform/$repo_name_aosp"
     echo "AOSP remote: $repo_aosp"
     echo "Revision to merge: $revision"
-    repo_remote=$(xmlstarlet sel -t -v "/manifest/project[@path='$path']/@remote" $snippet)
+    repo_remote=$(xmlstarlet sel -t -v "/manifest/project[@path='$path']/@remote" "$snippet")
 
-    pushd $path
+    pushd "$path"
+
+    repo checkout $ROM_VERSION || repo start $ROM_VERSION
 
     echo "Setting aosp remote"
     if ! git ls-remote aosp >/dev/null 2>/dev/null; then
-        git remote add aosp $repo_aosp || git remote set-url aosp $repo_aosp
+        git remote add aosp "$repo_aosp" || git remote set-url aosp "$repo_aosp"
     else
-        git remote set-url aosp $repo_aosp
+        git remote set-url aosp "$repo_aosp"
     fi
 
     if [ "$(git rev-parse --is-shallow-repository)" == "true" ]; then
         echo "Shallow repository detected, unshallowing first"
-        git fetch --unshallow $repo_remote
+        git fetch --unshallow "$repo_remote"
     fi
 
     echo "Fetching aosp"
     git fetch aosp
     echo "Merging aosp"
-    git merge $revision
+    git merge "$revision"
 
     echo
     popd
-done < <(xmlstarlet sel -t -v '/manifest/project[@merge-aosp="true"]/@path' $snippet)
+done < <(xmlstarlet sel -t -v '/manifest/project[@merge-aosp="true"]/@path' "$snippet" && echo)
 
-while read path; do
-    pushd $path
-    set +e
-    addXos
-    set -e
+while read -r path; do
+    pushd "$path"
     git push xos HEAD:$ROM_VERSION
     popd
-done < <(xmlstarlet sel -t -v '/manifest/project[@merge-aosp="true"]/@path' $snippet)
+done < <(xmlstarlet sel -t -v '/manifest/project[@merge-aosp="true"]/@path' "$snippet" && echo)
 
 echo "Everything done."
 
